fd114e2e78751b1253fe9f7d784262accc3b95a8
puji858 <61916553+puji858@users.noreply.github.com>
2020-06-30 22:53:05 +0700

    Add files via upload

---
+++ classanak/krywanharian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLab8.ClassInduk;
+
+namespace TugasLab8.ClassAnak
+{
+    public class KaryawanHarian : Karyawan
+    {
+        public int jumlahjamkerja { get; set; }
+        public int upahperJam { get; set; }
+        public override double Gaji()
+        {
+            return jumlahjamkerja * UpahPerJam;
+        }
+    }
+}
---
+++ classanak/sales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLab8.ClassInduk;
+
+namespace TugasLab8.ClassAnak
+{
+    public class Sales : Karyawan
+    {
+        public int jumlahpenjualan { get; set; }
+        public int komisi { get; set; }
+        public override double Gaji()
+        {
+            return jumlahpenjualan * Komisi;
+        }
+    }
+}
